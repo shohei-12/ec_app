49d5a064520accc6753ae2b44d0f1556ab455dce
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-09-30 23:46:09 +0900

    Implement user registration function (#12)

---
+++ src/components/UIkit/PrimaryButton.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles({
+  button: {
+    backgroundColor: "#4dd0e1",
+    color: "#000",
+    fontSize: 16,
+    height: 48,
+    marginBottom: 16,
+    width: 256,
+  },
+});
+
+type Props = {
+  label: string;
+  onClick: () => any;
+};
+const PrimaryButton: React.FC<Props> = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Button
+      className={classes.button}
+      variant="contained"
+      onClick={() => props.onClick()}
+    >
+      {props.label}
+    </Button>
+  );
+};
+
+export default PrimaryButton;
--- src/reducks/users/operations.ts
+++ src/reducks/users/operations.ts
@@ -1,6 +1,7 @@
 import { signInAction } from "./actions";
 import { push } from "connected-react-router";
 import { State } from "../store/types";
+import { auth, db, FirebaseTimestamp } from "../../firebase/index";
 
 export const signIn = () => {
   return async (dispatch: any, getState: any) => {
@@ -25,5 +26,55 @@ export const signIn = () => {
       );
       dispatch(push("/"));
     }
+  };
+};
+
+export const signUp = (
+  username: string,
+  email: string,
+  password: string,
+  confirmPassword: string
+) => {
+  return async (dispatch: any) => {
+    // Validation
+    if (
+      username === "" ||
+      email === "" ||
+      password === "" ||
+      confirmPassword === ""
+    ) {
+      alert("必須項目が未入力です。");
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      alert("パスワードが一致しません。もう1度お試しください。");
+      return false;
+    }
+
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        const user = result.user;
+
+        if (user) {
+          const uid = user.uid;
+          const timestamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            uid: uid,
+            username: username,
+            email: email,
+            role: "customer",
+            created_at: timestamp,
+            updated_at: timestamp,
+          };
+
+          db.collection("users")
+            .doc(uid)
+            .set(userInitialData)
+            .then(() => dispatch(push("/")));
+        }
+      });
   };
 };
