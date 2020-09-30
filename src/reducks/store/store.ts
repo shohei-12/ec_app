56ce6869abeef01c1f842655c1dbeb90229fe218
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-09-30 16:42:47 +0900

    Write operations with redux thunk (#7)

--- src/reducks/store/store.ts
+++ src/reducks/store/store.ts
@@ -4,6 +4,7 @@ import {
   applyMiddleware,
 } from "redux";
 import { connectRouter, routerMiddleware } from "connected-react-router";
+import thunk from "redux-thunk";
 import { UsersReducer } from "../users/reducers";
 
 export default function createStore(history: any) {
@@ -12,6 +13,6 @@ export default function createStore(history: any) {
       router: connectRouter(history),
       users: UsersReducer,
     }),
-    applyMiddleware(routerMiddleware(history))
+    applyMiddleware(routerMiddleware(history), thunk)
   );
 }
--- src/reducks/users/selectors.ts
+++ src/reducks/users/selectors.ts
@@ -4,3 +4,7 @@ import { State } from "../store/types";
 const usersSelector = (state: State) => state.users;
 
 export const getUserId = createSelector([usersSelector], (state) => state.uid);
+export const getUserName = createSelector(
+  [usersSelector],
+  (state) => state.username
+);
