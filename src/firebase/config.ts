c9591139faad1fe122471cf5e7484f81c01e3bc8
shohei-12 <shoheimaeda10@gmail.com>
2020-09-30 19:37:09 +0900

    Create firebase configuration file

---
+++ src/firebase/config.ts
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCA5i0S7EpwbWEM5SR35IhBagSp7riNpa4",
+  authDomain: "ec-app-4dabb.firebaseapp.com",
+  databaseURL: "https://ec-app-4dabb.firebaseio.com",
+  projectId: "ec-app-4dabb",
+  storageBucket: "ec-app-4dabb.appspot.com",
+  messagingSenderId: "946164617568",
+  appId: "1:946164617568:web:ad07dc1f7024cb9a7b8e05",
+  measurementId: "G-LX3456M1XN",
+};
