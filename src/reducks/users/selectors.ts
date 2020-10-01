705bcd4d4b0374767d39e385698960c3b037d306
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-10-01 13:01:45 +0900

    Create Auth Component (#15)

--- src/reducks/users/selectors.ts
+++ src/reducks/users/selectors.ts
@@ -3,7 +3,13 @@ import { State } from "../store/types";
 
 const usersSelector = (state: State) => state.users;
 
+export const getIsSignedIn = createSelector(
+  [usersSelector],
+  (state) => state.isSignedIn
+);
+
 export const getUserId = createSelector([usersSelector], (state) => state.uid);
+
 export const getUserName = createSelector(
   [usersSelector],
   (state) => state.username
