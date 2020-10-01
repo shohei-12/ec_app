8b762a19bf0e7af98fbbd476e4c8a92614442eff
shohei-12 <shoheimaeda10@gmail.com>
2020-10-01 20:00:51 +0900

    Create product registration editing form

---
+++ src/templates/ProductEdit.tsx
@@ -0,0 +1,97 @@
+import React, { useCallback, useState } from "react";
+import { PrimaryButton, SelectBox, TextInput } from "../components/UIkit";
+
+const ProductEdit: React.FC = () => {
+  const [name, setName] = useState(""),
+    [description, setDescription] = useState(""),
+    [category, setCategory] = useState(""),
+    [gender, setGender] = useState(""),
+    [price, setPrice] = useState("");
+
+  const inputName = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setName(event.target.value);
+    },
+    [setName]
+  );
+
+  const inputDescription = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setDescription(event.target.value);
+    },
+    [setDescription]
+  );
+
+  const inputPrice = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      setPrice(event.target.value);
+    },
+    [setPrice]
+  );
+
+  const categories = [
+    { id: "tops", name: "トップス" },
+    { id: "shirts", name: "シャツ" },
+    { id: "pants", name: "パンツ" },
+  ];
+
+  const genders = [
+    { id: "all", name: "すべて" },
+    { id: "male", name: "メンズ" },
+    { id: "female", name: "レディース" },
+  ];
+
+  return (
+    <section>
+      <h2 className="u-text__headline u-text-center">商品の登録・編集</h2>
+      <div className="c-section-container">
+        <TextInput
+          fullWidth={true}
+          label="商品名"
+          multiline={false}
+          required={true}
+          rows="1"
+          value={name}
+          type="text"
+          onChange={inputName}
+        />
+        <TextInput
+          fullWidth={true}
+          label="商品説明"
+          multiline={true}
+          required={true}
+          rows="5"
+          value={description}
+          type="text"
+          onChange={inputDescription}
+        />
+        <SelectBox
+          label="カテゴリー"
+          required={true}
+          options={categories}
+          select={setCategory}
+          value={category}
+        />
+        <SelectBox
+          label="性別"
+          required={true}
+          options={genders}
+          select={setGender}
+          value={gender}
+        />
+        <TextInput
+          fullWidth={true}
+          label="価格"
+          multiline={false}
+          required={true}
+          rows="1"
+          value={price}
+          type="number"
+          onChange={inputPrice}
+        />
+      </div>
+    </section>
+  );
+};
+
+export default ProductEdit;
--- src/templates/index.ts
+++ src/templates/index.ts
@@ -1,4 +1,5 @@
 export { default as Home } from "./Home";
+export { default as ProductEdit } from "./ProductEdit";
+export { default as Reset } from "./Reset";
 export { default as SignIn } from "./SignIn";
 export { default as SignUp } from "./SignUp";
-export { default as Reset } from "./Reset";
