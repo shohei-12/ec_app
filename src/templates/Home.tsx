14d3e7862044fd596b885c584958945314b059c1
shohei-12 <65577006+shohei-12@users.noreply.github.com>
2020-10-01 14:04:27 +0900

    Implement signout feature (#16)

--- src/templates/Home.tsx
+++ src/templates/Home.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { signOut } from "../reducks/users/operations";
 import { State } from "../reducks/store/types";
 import { getUserId, getUserName } from "../reducks/users/selectors";
 
 const Home: React.FC = () => {
+  const dispatch = useDispatch();
   const selector = useSelector((state: State) => state);
   const uid = getUserId(selector);
   const username = getUserName(selector);
@@ -13,6 +15,7 @@ const Home: React.FC = () => {
       <h2>Home</h2>
       <p>ユーザーID：{uid}</p>
       <p>ユーザー名：{username}</p>
+      <button onClick={() => dispatch(signOut())}>SIGN OUT</button>
     </div>
   );
 };
