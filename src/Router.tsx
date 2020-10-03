812ae4b5d965641c7be93166a70a9944baba677b
shohei-12 <shoheimaeda10@gmail.com>
2020-10-03 21:42:36 +0900

    Create product details page

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 import { Switch, Route } from "react-router";
-import { ProductEdit, ProductList, Reset, SignIn, SignUp } from "./templates";
+import {
+  ProductDetail,
+  ProductEdit,
+  ProductList,
+  Reset,
+  SignIn,
+  SignUp,
+} from "./templates";
 import Auth from "./Auth";
 
 const Router: React.FC = () => {
@@ -12,6 +19,7 @@ const Router: React.FC = () => {
       <Auth>
         <Route exact path="(/)?" component={ProductList} />
         <Route path="/product/edit(/:id)?" component={ProductEdit} />
+        <Route exact path="/product/:id" component={ProductDetail} />
       </Auth>
     </Switch>
   );
--- src/reducks/products/types.ts
+++ src/reducks/products/types.ts
@@ -13,6 +13,17 @@ export type Products = {
   sizes: { size: string; quantity: number }[];
 }[];
 
+export type Product = {
+  id?: string;
+  name?: string;
+  description?: string;
+  category?: string;
+  gender?: string;
+  images?: { id: string; path: any }[];
+  price?: string;
+  sizes?: { size: string; quantity: number }[];
+};
+
 export interface ProductAction {
   type: string;
   payload: Products;
