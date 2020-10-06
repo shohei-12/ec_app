eeb107619190034dd12d5609ddd4ae2315269f93
shohei-12 <shoheimaeda10@gmail.com>
2020-10-06 17:33:33 +0900

    Create OrderedProducts Component

--- src/components/Products/OrderHistoryItem.tsx
+++ src/components/Products/OrderHistoryItem.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { TextDetail } from "../UIkit";
+import { OrderedProducts } from ".";
 import { Order } from "../../reducks/users/types";
 import Divider from "@material-ui/core/Divider";
 
@@ -45,6 +46,9 @@ const OrderHistoryItem: React.FC<Props> = (props) => {
       <TextDetail label="注文日時" value={orderedDatetime} />
       <TextDetail label="発送予定日" value={shippingDate} />
       <TextDetail label="注文金額" value={price} />
+      {order.products.length > 0 && (
+        <OrderedProducts products={order.products} />
+      )}
       <div className="module-spacer--extra-extra-small" />
       <Divider />
     </div>
--- src/components/Products/index.ts
+++ src/components/Products/index.ts
@@ -2,6 +2,7 @@ export { default as CartListItem } from "./CartListItem";
 export { default as ImageArea } from "./ImageArea";
 export { default as ImagePreview } from "./ImagePreview";
 export { default as ImageSwiper } from "./ImageSwiper";
+export { default as OrderedProducts } from "./OrderedProducts";
 export { default as OrderHistoryItem } from "./OrderHistoryItem";
 export { default as ProductCard } from "./ProductCard";
 export { default as SetSizeArea } from "./SetSizeArea";
